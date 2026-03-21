2fe58f59221190371bd77475441c7ab8b180811d
Eduardo Domingos <eduardodomingos2077@gmail.com>
2026-03-21 00:12:01 -0300

    feat (backend): validação de envs

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,32 +1,20 @@
 import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
-import { ConfigService, ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
-import { AuthModule } from './auth/auth.module';
-import * as Joi from 'joi';
+import { envValidationSchema } from './config/env.validations';
+import { ConfigModule } from '@nestjs/config';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       ignoreEnvFile: true,
-      validationSchema: Joi.object({
-        HOST: Joi.string().required(),
-        PORT: Joi.string().required(),
-        MONGO_URI: Joi.string().uri().required(),
-        PEPPER: Joi.string().required(),
-      }),
+      validationSchema: envValidationSchema,
     }),
-    MongooseModule.forRootAsync({
-      inject: [ConfigService],
-      useFactory: (configService: ConfigService) => ({
-        uri: configService.getOrThrow<string>('MONGO_URI'),
-      }),
-    }),
+    DatabaseModule,
     UsersModule,
-    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,37 +1,22 @@
 import { NestFactory } from '@nestjs/core';
-import {
-  FastifyAdapter,
-  NestFastifyApplication,
-} from '@nestjs/platform-fastify';
+import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import { AppModule } from './app.module';
-import fastifyCookie from '@fastify/cookie';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestFastifyApplication>(
-    AppModule,
-    new FastifyAdapter(),
-  );
+  const app = await NestFactory.create(AppModule);
+
+  const configService = app.get(ConfigService);
+  const port = configService.getOrThrow<number>('PORT');
 
   app.useGlobalPipes(
     new ValidationPipe({
-      whitelist: true, // remove propriedades que não estão no obj DTO
-      forbidNonWhitelisted: true, // aponta erro pra prop inexistente no DTO
-      transform: true, // converte dados dos params da requisição e DTOs
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
     }),
   );
 
-  const configService = app.get(ConfigService);
-
-  await app.register(fastifyCookie, {
-    secret: configService.getOrThrow<string>('COOKIE_SECRET'),
-  });
-
-  const port: number = configService.getOrThrow<number>('PORT');
-  const host: string = configService.getOrThrow<string>('HOST');
-
-  await app.listen(port, host);
-  console.log(`🚀 Server running on http://${host}:${port}`);
+  await app.listen(port);
 }
 void bootstrap();
