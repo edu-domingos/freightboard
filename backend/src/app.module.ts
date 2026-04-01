d621e6c45fd8cd5e4ef6e54d53d0e7e825feadb7
Eduardo Domingos <eduardodomingos2077@gmail.com>
2026-03-31 23:07:08 -0300

    feat (backend): configurando redis/envs

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,18 +2,70 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
-import { envValidationSchema } from './config/env.validations';
-import { ConfigModule } from '@nestjs/config';
-import { DatabaseModule } from './database/database.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import Joi from 'joi';
+import { AuthModule } from './auth/auth.module';
+import { CacheModule } from '@nestjs/cache-manager';
+import { redisStore } from 'cache-manager-ioredis-yet';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       ignoreEnvFile: true,
-      validationSchema: envValidationSchema,
+      validationSchema: Joi.object({
+        DB_HOST: Joi.string().required(),
+        DB_PORT: Joi.number().required(),
+        DB_USER: Joi.string().required(),
+        DB_PASSWORD: Joi.string().required(),
+        DB_NAME: Joi.string().required(),
+        DB_AUTOLOAD_ENTITIES: Joi.boolean()
+          .truthy('true')
+          .falsy('false')
+          .required(),
+        DB_SYNCRONIZE: Joi.boolean().truthy('true').falsy('false').required(),
+        REDIS_HOST: Joi.string().required(),
+        REDIS_PORT: Joi.number().required(),
+        PEPPER: Joi.string().required(),
+        JWT_SECRET: Joi.string().required(),
+        JWT_ALGORITHM: Joi.string().required(),
+        JWT_AUDIENCE: Joi.string().required(),
+        JWT_ISSUER: Joi.string().required(),
+        JWT_EXPIRES_IN: Joi.string().required(),
+        JWT_REFRESH_EXPIRES_IN: Joi.string().required(),
+        NODE_ENV: Joi.string()
+          .valid('development', 'production')
+          .default('development'),
+      }),
     }),
-    DatabaseModule,
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'postgres',
+        host: config.get('DB_HOST'),
+        port: config.get('DB_PORT'),
+        username: config.get('DB_USER'),
+        password: config.get('DB_PASSWORD'),
+        database: config.get('DB_NAME'),
+        autoLoadEntities: config.get('DB_AUTOLOAD_ENTITIES'),
+        synchronize: config.get('DB_SYNCRONIZE'),
+        logging: config.get('LOGGING'),
+      }),
+    }),
+    CacheModule.registerAsync({
+      isGlobal: true,
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        store: redisStore,
+        socket: {
+          host: configService.get<string>('REDIS_HOST'),
+          port: Number(configService.get<number>('REDIS_PORT')),
+        },
+        ttl: 60,
+      }),
+    }),
+    AuthModule,
     UsersModule,
   ],
   controllers: [AppController],
