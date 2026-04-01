d184a497c8e200a09d37a23ab75e20bdf53342fc
Eduardo Domingos <eduardodomingos2077@gmail.com>
2026-03-31 23:07:26 -0300

    feat (backend): configurando fastify

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,12 +2,19 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter({ logger: true }),
+  );
 
   const configService = app.get(ConfigService);
-  const port = configService.getOrThrow<number>('PORT');
+  const port = Number(configService.get<number>('PORT'));
 
   app.useGlobalPipes(
     new ValidationPipe({
@@ -17,6 +24,8 @@ async function bootstrap() {
     }),
   );
 
-  await app.listen(port);
+  app.enableCors();
+
+  await app.listen(port, '0.0.0.0');
 }
 void bootstrap();
