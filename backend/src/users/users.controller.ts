0a9f55b9f3b1102cf980d7b060d5e50e2c04f5ba
Eduardo Domingos <eduardodomingos2077@gmail.com>
2026-04-01 21:43:39 -0300

    feat (backend): implementar crud básico para usuário com adequação ao redis

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -1,38 +1,55 @@
 import {
   Body,
   Controller,
+  Delete,
   Get,
   HttpCode,
   HttpStatus,
   Patch,
+  Post,
   Req,
   UseGuards,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
-import type { FastifyRequest } from 'fastify';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
-  /** atualizar usuário */
-  @UseGuards(JwtAuthGuard)
-  @Patch('me')
-  @HttpCode(HttpStatus.OK)
-  async update(
-    @Body() updateUserDto: UpdateUserDto,
-    @Req() request: FastifyRequest,
-  ) {
-    return this.usersService.update(request.user.id, updateUserDto);
-  }
-
-  /** trazer dados do usuário */
   @UseGuards(JwtAuthGuard)
   @Get('me')
   @HttpCode(HttpStatus.OK)
-  async findById(@Req() request: FastifyRequest) {
-    return this.usersService.findById(request.user.id);
+  async findById(@Req() request) {
+    return request.user;
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Patch('me')
+  @HttpCode(HttpStatus.OK)
+  async update(@Body() updateUserDto: UpdateUserDto, @Req() request) {
+    return this.usersService.update(request.user, updateUserDto);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Post('me/avatar')
+  @HttpCode(HttpStatus.OK)
+  async uploadAvatar(@Req() req) {
+    if (!req.files || req.files.length === 0)
+      throw new Error('Nenhum arquivo enviado');
+
+    const file = req.files[0]; // pegamos o primeiro arquivo enviado
+
+    const updatedUser = await this.usersService.updateAvatar(req.user, file);
+
+    return { avatar: updatedUser.avatar };
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Delete('me')
+  @HttpCode(HttpStatus.OK)
+  async deleteById(@Req() request) {
+    return this.usersService.deleteById(request.user);
   }
 }
