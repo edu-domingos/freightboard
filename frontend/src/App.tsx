44acc841490fa95f0f5a93fe3ff94ddaed59509b
BMS <munizbruno29@gmail.com>
2026-04-01 23:13:46 -0300

    feat(frontend): Criação da tela Superadmin e seus componentes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,25 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import Login from "./components/Login";
 import Cadastro from "./components/Cadastro";
 import Home from "./components/Home";
 import Configuracoes from "./components/Configuracoes";
 import Perfil from "./components/Perfil";
+import SuperAdmin from "./pages/SuperAdmin/SuperAdmin";
 
 function App() {
-  
   return (
     <BrowserRouter>
+      <ToastContainer />
+
       <Routes>
-        <Route path="/" element={<Login/>} />
-        { <Route path="/cadastro" element={<Cadastro/>} /> }
-        { <Route path="/home" element={<Home/>} /> }
-        { <Route path="/configuracoes" element={<Configuracoes/>} /> }
-        { <Route path="/perfil" element={<Perfil/>} /> }
+        <Route path="/" element={<Login />} />
+        {<Route path="/cadastro" element={<Cadastro />} />}
+        {<Route path="/home" element={<Home />} />}
+        {<Route path="/configuracoes" element={<Configuracoes />} />}
+        {<Route path="/perfil" element={<Perfil />} />}
+        {<Route path="/superadmin" element={<SuperAdmin />} />}
       </Routes>
     </BrowserRouter>
   );
---
+++ frontend/src/pages/SuperAdmin/components/SystemStatus.tsx
@@ -0,0 +1,10 @@
+export default function SystemStatus() {
+  return (
+    <div className="adminCard">
+      <h2>Status do Sistema</h2>
+      <p>API: 🟢 Online</p>
+      <p>Banco: 🟢 Conectado</p>
+      <p>Uptime: 2h 32min</p>
+    </div>
+  );
+}
